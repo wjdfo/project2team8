0ad6a79140f2ac15c93927360187ea07e0152b91
JungRae Jo <wjdfoek3@knu.ac.kr>
2024-03-30 14:09:32 +0900

    Feat: 사업보고서 크롤링 기능

--- DART/OpenDartReader/knuturn.py
+++ DART/OpenDartReader/knuturn.py
@@ -1,9 +1,61 @@
 import pandas as pd
 import json
-import knuturn_method as km
+import knuturn_method
 
-a = km.knuturn('./dart_api_key.txt') # 생성자 parameter : api key text file path
-d = a.getReportCode('./kospi_top_30.txt') # param : 회사이름 담긴 text file path
+#필요한 함수 주석 해제해서 쓰세요
 
-with open('./20230101_20240329_report.json', 'w', encoding = 'utf-8')  as f :
-    json.dump(d, f, ensure_ascii=False, indent = '\t')
+a = knuturn_method.knuturn('./dart_api_key.txt') # param : api key text file path
+
+companies = []
+
+f = open('./kospi_top_30.txt', 'r', encoding = 'utf-8')
+lines = f.readlines()
+
+for line in lines :
+    companies.append(line.strip())
+
+
+# 회사 고유코드 얻어오는 method <- report_list.json 파일 없을 때 사용
+# report_list = a.getReportCode(companies) # param : 회사 리스트
+
+# with open('./20230101_20240329_report_list.json', 'w', encoding = 'utf-8')  as f : # report list json 파일로 저장
+#     json.dump(report_list, f, ensure_ascii=False, indent = '\t')
+
+
+
+#이미 report_list.json 파일 있는 경우 사용
+with open('./20230101_20240329_report_list.json', 'r', encoding = 'utf-8')  as f :
+    report_list = json.load(f)
+
+
+
+# 회사 고유코드로 사업보고서 url 가져오는 method <- report_url.json 파일 없을 때 사용
+# report_url = a.getReportURL(report_list) # param : report num dict
+
+# with open('./20230101_20240329_report_url.json', 'w', encoding = 'utf-8')  as f : # report url json 파일로 저장
+#     json.dump(report_url, f, ensure_ascii=False, indent = '\t')
+
+
+
+#이미 report_url.json 파일 있는 경우 사용
+with open('./20230101_20240329_report_url.json', 'r', encoding = 'utf-8') as f :
+    report_url = json.load(f)
+
+report_data = a.getReportData(report_url)
+
+with open('./20230101_20240329_report_data.json', 'w', encoding = 'utf-8') as f :
+    json.dump(report_data, f, ensure_ascii = False, indent = '\t')
+
+
+# xml to json
+# pip install xmltodict
+
+# import xmltodict
+
+# jsonString = json.dumps(xmltodict.parse(xml_text), indent='\t', ensure_ascii=False)
+
+# print("\nJSON output(output.json):")
+# print(jsonString)
+
+# with open("xml_to_json.json", 'w', encoding='utf-8') as f:
+#     f.write(jsonString)
--- DART/OpenDartReader/knuturn_method.py
+++ DART/OpenDartReader/knuturn_method.py
@@ -1,24 +1,21 @@
 import OpenDartReader
 import pandas as pd
 import json
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
 class knuturn :
-    def __init__(self, file: str) :
+    def __init__(self, file: str) : #api key text파일 경로
         with open('./dart_api_key.txt', 'r') as f :
             api_key = f.readline()
         self.dart = OpenDartReader(api_key)
 
-    def getReportCode(self, corp_file: str) : #공시보고서 코드를 회사마다 dictionary에 담아서 return
-        companies = []
+    def getReportCode(self, comps: list) : #공시보고서 코드를 회사마다 dictionary에 담아서 return
         d = {}
 
-        f = open('./kospi_top_30.txt', 'r', encoding = 'utf-8')
-        lines = f.readlines()
-
-        for line in lines :
-            companies.append(line.strip())
-
-        for comp in companies :
+        for comp in comps :
             print(comp, end = " ")
             try : # dart.list 함수 호출했을 때, data 없는 경우에도 exception raise하지 않고 {"status":"013","message":"조회된 데이타가 없습니다."} 출력하는 오류 있습니다.
                 report_list = self.dart.list(comp, start = '2023-01-01', end = '2024-03-28', kind = 'A')
@@ -31,3 +28,52 @@ class knuturn :
                 continue
         
         return d
+    
+    def getReportURL(self, report_list: dict) : #report_list 받아서 공시보고서 url 생성
+        report_url = {}
+
+        for corp_code in report_list.keys() :
+            report_url[corp_code] = {}
+
+            for report_num in report_list[corp_code] :
+                report_url[corp_code][report_num] = {}
+                for idx, row in self.dart.sub_docs(report_num).iterrows() :
+                    report_url[corp_code][report_num][row['title']] = row['url']
+            
+        return report_url
+    
+    def getReportData(self, report_url: dict) : #report_url_list 받아서 url에서 text 가져옴
+        
+        report_data = {}
+        driver = webdriver.Chrome()
+
+        fp = open('./crawling_output_try_2.txt', 'a', encoding = 'utf-8')
+
+        for corp_code in report_url.keys() :
+            report_data[corp_code] = {}
+
+            for report_num in report_url[corp_code] :
+                report_data[corp_code][report_num] = {}
+
+                for title in report_url[corp_code][report_num] :
+                    url = report_url[corp_code][report_num][title]
+                    report_data[corp_code][report_num][title] = []
+                    fp.write(title + "\n")
+
+                    driver.get(url)
+                    time.sleep(2)
+
+                    report_string = driver.find_element(By.TAG_NAME, 'body').text
+                    lines = report_string.splitlines()
+
+                    for line in lines :
+                        if len(line) < 1 : # 빈 줄 삭제
+                            continue
+                        report_data[corp_code][report_num][title].append(line + "\n")
+                        fp.write(line + "\n")
+                    fp.write("\n")
+                    # print(report_string)
+
+        driver.quit()
+
+        return report_data
