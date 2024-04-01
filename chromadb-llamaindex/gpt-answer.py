27d5bbeae6faa8c63b15e72886bfb727f7c7d0f5
JungRae Jo <wjdfoek3@knu.ac.kr>
2024-04-02 07:43:10 +0900

    Feat: DART chatbot 기능 추가

--- chromadb-llamaindex/gpt-answer.py
+++ chromadb-llamaindex/gpt-answer.py
@@ -22,7 +22,7 @@ COLLECTION_NAME = 'whole_240331_0901_json'
 EMBEDDING_MODLE = 'sentence-transformers/all-mpnet-base-v2'
 
 # 추출된 파일 어느 폴더에 있는가?
-DATA_PATH = 'data'
+# DATA_PATH = 'data'
 
 # 어떤 item들을 저장할 것인가 ? default는 전부 다
 EXTRACTED_ITEMS = [ "1", "1A", "1B", "2", "3", "4", "5", "6", "7", "7A",
@@ -32,7 +32,7 @@ EXTRACTED_ITEMS = [ "1", "1A", "1B", "2", "3", "4", "5", "6", "7", "7A",
 API_KEY = ''
 ###########################
 
-# json 에 해당 키값이 존재하는가
+# json 에 해당 키값이 존재하는가ㅜ
 def is_json_key_present(json, key):
     try:
         buf = json[key]
@@ -41,7 +41,95 @@ def is_json_key_present(json, key):
 
     return True
 
+def edgar(path: str, option: int) :
+    if os.path.exists(path) :
+        file_list = os.listdir(path)
 
+    else :
+        print(f'There is no \"{path}\" folder')
+        exit()
+
+    documents = []
+
+    for file_name in file_list :
+        file_path = os.path.join(path, file_name)
+
+        with open(file_path, 'r', encoding = 'utf-8') as f :
+            file = json.load(f)
+
+        # field 값 설정 후 document 화 하기
+        file_name = file['company']+file['period_of_report']
+
+        if option == 1 :
+            for items in EXTRACTED_ITEMS:
+                if is_json_key_present(file,f'item_{items}') == False : continue
+                text = file[f'item_{items}']
+                included_items = f'item_{items}'
+                doc_id = 'EDGAR_'+file['filing_type']+'_'+file['cik']+'_'+file['period_of_report']+':'+included_items
+
+                doc = Document(text = text,doc_id = doc_id, metadata = {'filename' : file_name, 'items' : included_items, 
+                                                    'filing_type' : file['filing_type'], 'htm_filing_link' : file['htm_filing_link'], 'report_date' : file['period_of_report']  })
+                documents.append(doc)
+
+        elif option == 2 :
+            # v == 2 : 전체 내용 다 저장하기
+            text = ''
+            included_items =''
+            for items in EXTRACTED_ITEMS:
+                if is_json_key_present(file,f'item_{items}') == False : continue
+                text += file[f'item_{items}']
+                included_items += f'item_{items}, '
+            included_items = included_items[0:-2]
+            doc_id = 'EDGAR_'+file['filing_type']+'_'+file['cik']+'_'+file['period_of_report']+':'+included_items
+
+            doc = Document(text = text,doc_id = doc_id, metadata = {'filename' : file_name, 'items' : included_items, 
+                                                'filing_type' : file['filing_type'], 'htm_filing_link' : file['htm_filing_link'], 'report_date' : file['period_of_report']  })
+            documents.append(doc)
+
+    return documents
+
+def dart(path: str, option: int) :
+    if os.path.exists(path) : file = os.listdir(path)[0]
+    else :
+        print(f'There is no \"{path}\" folder')
+        exit()
+
+    file_path = os.path.join(path, file)
+    documents = []
+
+    with open(file_path, 'r', encoding = 'utf-8') as f :
+        data = json.load(f)
+
+    if option == 1 : # title별로 문서화
+        for corp_name in data.keys() : #회사명
+            for report_number in data[corp_name].keys() : #보고서 번호
+                print(f"회사명 : {corp_name}, 보고서 번호 : {report_number}")
+                for title in data[corp_name][report_number].keys() : #보고서 하위 목차
+                    text = ""
+                    file_name = corp_name + "_" + report_number + "_" + title
+                    for element in data[corp_name][report_number][title] :
+                        text += element
+                    doc_id = "DART_" + corp_name + "_" + report_number + "_" + title
+                    doc = Document(text = text, doc_id = doc_id, metadata = {'filename' : file_name, "corp_name" : corp_name,
+                                                                                "report_number" : report_number, 'title' : title})
+                    documents.append(doc)
+
+    elif option == 2 : #사업보고서별로 문서화
+        for corp_name in data.keys() : #회사명
+            for report_number in data[corp_name].keys() : #보고서 번호
+                print(f"회사명 : {corp_name}, 보고서 번호 : {report_number}")
+                text = ""
+                file_name = corp_name + '_' + report_number
+                doc_id = "DART_" + corp_name + '_' + report_number
+                for title in data[corp_name][report_number].keys() : #보고서 하위 목차
+                    for element in data[corp_name][report_number][title] :
+                        text += element
+
+                doc = Document(text = text, doc_id = doc_id, metadata = {'filename' : file_name, "corp_name" : corp_name,
+                                                                        "report_number" : report_number})
+                documents.append(doc)
+
+    return documents
 
 def main():
     # API_KEY 와 llm 세팅. 터미널에서도 수정 가능
@@ -75,8 +163,6 @@ def main():
     vector_store = ChromaVectorStore(chroma_collection=collection)
     storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
-
-
     while(1):
         i = int(input('store from document data & answer (1), answer from existing vector (2) : '))
         
@@ -85,55 +171,24 @@ def main():
             print('(1) selected')
             
             v = int(input('store separate by each items (1), store whole report (2) : '))
+
             if v == 1 : print('separate by each items - selected')
             elif v == 2 : print('whole report - selected')
             else : continue
 
             documents = []
-            
-            # data 가져오기
-            if os.path.exists(DATA_PATH):
-                file_list = os.listdir(DATA_PATH)
-            else :
-                print(f'There is no {DATA_PATH} folder')
-                exit()
 
-            # 각 파일에 대해,
-            for file_name in file_list:
-                file_path = os.path.join(DATA_PATH, file_name)
-
-                # json open
-                with open(file_path) as fin:
-                    file = json.load(fin)
-
-                # field 값 설정 후 document 화 하기
-                file_name = file['company']+file['period_of_report']
-
-                # v == 1 : item 별로 나누어서 document 화 하기
-                if v == 1 :
-                    for items in EXTRACTED_ITEMS:
-                        if is_json_key_present(file,f'item_{items}') == False : continue
-                        text = file[f'item_{items}']
-                        included_items = f'item_{items}'
-                        doc_id = 'EDGAR_'+file['filing_type']+'_'+file['cik']+'_'+file['period_of_report']+':'+included_items
-
-                        doc = Document(text = text,doc_id = doc_id, metadata = {'filename' : file_name, 'items' : included_items, 
-                                                            'filing_type' : file['filing_type'], 'htm_filing_link' : file['htm_filing_link'], 'report_date' : file['period_of_report']  })
-                        documents.append(doc)
-                # v == 2 : 전체 내용 다 저장하기
-                if v == 2 :
-                    text = ''
-                    included_items =''
-                    for items in EXTRACTED_ITEMS:
-                        if is_json_key_present(file,f'item_{items}') == False : continue
-                        text += file[f'item_{items}']
-                        included_items += f'item_{items}, '
-                    included_items = included_items[0:-2]
-                    doc_id = 'EDGAR_'+file['filing_type']+'_'+file['cik']+'_'+file['period_of_report']+':'+included_items
-
-                    doc = Document(text = text,doc_id = doc_id, metadata = {'filename' : file_name, 'items' : included_items, 
-                                                        'filing_type' : file['filing_type'], 'htm_filing_link' : file['htm_filing_link'], 'report_date' : file['period_of_report']  })
-                    documents.append(doc)
+            while True :
+                n = int(input("edgar (1), dart (2) : "))
+                if n == 1 :
+                    documents = edgar("data-edgar", v)
+                    break
+                elif n == 2 :
+                    documents = dart("data-dart", v)
+                    break
+                else :
+                    print("Please enter again.")
+                    continue
 
             index = VectorStoreIndex.from_documents(
                 documents, storage_context=storage_context, embed_model=embed_model
@@ -152,18 +207,37 @@ def main():
         else:
             print('do again')
     
+    while True :
+        option = int(input("CHAT: EDGAR mode (1), DART mode (2) : "))
+        if option == 1 :
+            print("-------EDGAR mode-------")
+            context = (
+                'You are a assistant to anaylsis EDGAR SEC filings. You have to give client more specific information including exact examples, current situation for client to have information to invest',
+                'You have to answer in Korean, and if you find there is no exact correspondance of English word to Korean word, you can write both korean & english terms.',
+                'and give informations as much as you know'
+            )
+            break
+        elif option == 2 :
+            print("-------DART mode-------")
+            context = (
+                '너는 DART의 사업보고서를 분석해주는 조력자야. 너는 고객들에게 정확한 예시, 투자를 위한 정보를 가지고 싶어하는 고객을 위한 현상황 등의 자세한 정보를 제공해야해.',
+                '답변은 한국어로 하고, 너가 아는대로 최대한 많은 정보를 자세하게 제공해줘'
+            )
+            break
+        else : continue
+
     # query
     chat_engine = index.as_chat_engine(
         chat_mode='context',
-        system_prompt = (
-            'You are a assistant to anaylsis EDGAR SEC filings. You have to give client more specific information including exact examples, current situation for client to have information to invest',
-            'You have to answer in Korean, and if you find there is no exact correspondance of English word to Korean word, you can write both korean & english terms.',
-            'and give informations as much as you know'
-        )
+        system_prompt = context
     )
-    response = chat_engine.chat("Categorize [Amazon]'s  [2021] year [10-K] report. at least list 5 of them")
 
-    print(response)
+    while True :
+        question = input("Message to Knuturn (Please enter \'q\' to quit) ~ ")
+        if question == 'q' or question == 'Q' or question == 'ㅂ' : break
+
+        response = chat_engine.chat(question)
+        print(response)
 
 if __name__ == '__main__':
     main()
