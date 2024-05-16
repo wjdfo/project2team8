a675a7cc34193845aa761cff2f76122b639e392f
Sunghan Shin <crossrunway01@gmail.com>
2024-05-16 13:27:41 +0900

    feat: API 연동 코드 추가

--- BE/App/App/views.py
+++ BE/App/App/views.py
@@ -6,18 +6,54 @@ class API(View):
     @classmethod
     def chatbot_response(cls, request):
         try:
+            isDart = True
+            corp_name = json.loads(request.body)["corpName"]
             question = json.loads(request.body)["question"]
+            response = chatbot.getResponse(isDart, question)
             return JsonResponse({"message": f"Question: {question}, Answer: chatbot response"}, status=200)
         except:
             return HttpResponseBadRequest("Invalid Request")
     
-    def corporations_list(self):
+    def corporations_list(self, request):
+        try:
+            isDart = True
+            corp_name = json.loads(request.body)["corpName"]
+            list = chatbot.getCorpList(isDart)
+            return JsonResponse({"message": f"List: {list}"}, status=200)
+        except:
+            return HttpResponseBadRequest("Invalid Request")
 
-        return JsonResponse({"message": "corporations list"}, status=200)
-
-    def summary(self, corporation, cik):
-        print(corporation, cik)
-        return JsonResponse({"message": f"summary for corporation: {corporation}, cik: {cik}"}, status=200)
+    def summary(self, request):
+        try:
+            corp_name = json.loads(request.body)["corpName"]
+            reportNum = json.loads(request.body)["reportNum"]
+            summary = chatbot.getCorpSummary(corp_namee, reportNum)
+            return JsonResponse({"message": f"Summary: {summary}"}, status=200)
+        except:
+            return HttpResponseBadRequest("Invalid Request")
+    
+    def report(self, request):
+        # 특정 기업 보고서 원문 url 출력
+        try:
+            isDart = True
+            corp_name = json.loads(request.body)["corpName"]
+            fromDate = json.loads(request.body)["fromDate"]
+            toDate = json.loads(request.body)["toDate"]
+            list = chatbot.getCorpList(isDart, corp_name, date)
+            return JsonResponse({"message": f"List: {list}"}, status=200)
+        except:
+            return HttpResponseBadRequest("Invalid Request")
+        
+    def compare(self, request):
+        # 보고서끼리 비교
+        try:
+            isDart = True
+            corpName = json.loads(request.body)["corpName"]
+            targetCorpName = json.loads(request.body)["targetCorpName"]
+            report = chatbot.getCorpList(isDart, corpList)
+            return JsonResponse({"message": f"Report: {report}"}, status=200)
+        except:
+            return HttpResponseBadRequest("Invalid Request")
     
 class DartAPI(View):
     @classmethod
