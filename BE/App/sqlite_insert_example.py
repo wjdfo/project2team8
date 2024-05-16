04a95b4b54a7b28cbc635f97bc5a5b796478096f
Sunghan Shin <crossrunway01@gmail.com>
2024-05-16 21:31:28 +0900

    feat: 엑셀파일 읽어서 디비 적재 자동화 기능 추가

--- BE/App/sqlite_insert_example.py
+++ BE/App/sqlite_insert_example.py
@@ -3,11 +3,19 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "App.settings")
 import django
 django.setup()
 
+import pandas as pd
+
 from App.models import App
 
-test = App(corp_name="현대자동차", corp_num="1234", corp_jamo="ㅎㄷㅈㄷㅊ")
-test.save()
+current_dir = os.getcwd()
+file_path = current_dir + '/BE/App/노가다.xlsx'
 
+df = pd.read_excel(file_path)
+
+for index, row in df.iterrows():
+    test = App(corp_name=row.iloc[1], corp_num=row.iloc[0], corp_jamo=row.iloc[2])
+    test.save()
+    
 all_apps = App.objects.all()
 for app in all_apps:
     print(app.corp_name, app.corp_num, app.corp_jamo)
