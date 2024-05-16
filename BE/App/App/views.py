6964928e992780af5099bfea5406f616e87fa968
justgotothedesk <wlscjf3877@naver.com>
2024-05-16 16:21:38 +0900

    feat: Chatbot 모듈 import 및 기능 추가

--- BE/App/App/views.py
+++ BE/App/App/views.py
@@ -1,91 +1,104 @@
 from django.http import *
 from django.views import View
-import Chatbot
 import json
 
+import sys, os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+sys.path.append(parent_dir)
+from Chatbot import *
+from Dart import *
+
+# Dart인지 아닌지 판단
+def dart_check(corpName, dart_list):
+    if corpName in dart_list:
+        return True
+    return False
+
 class API(View):
+    # ChatBot 답변
     @classmethod
     def chatbot_response(cls, request):
         try:
-            isDart = True
-            corp_name = json.loads(request.body)["corpName"]
+            corpName = json.loads(request.body)["corpName"]
             question = json.loads(request.body)["question"]
-            response = Chatbot.getResponse(isDart, question)
-            return JsonResponse({"message": f"Question: {question}, Answer: chatbot response"}, status=200)
-        except:
-            return HttpResponseBadRequest("Invalid Request")
-    
-    def corporations_list(self, request):
-        try:
-            isDart = True
-            corp_name = json.loads(request.body)["corpName"]
-            list = Chatbot.getCorpList(isDart)
-            return JsonResponse({"message": f"List: {list}"}, status=200)
-        except:
-            return HttpResponseBadRequest("Invalid Request")
+            dart = Dart()
+            dart_list = dart.getCorpList()
+            isDart = dart_check(corpName, dart_list)
 
-    def summary(self, request):
+            chatbot = Chatbot()
+            response = chatbot.getResponse(question, isDart)
+
+            return JsonResponse({"response": response}, status=200)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+    # 특정 기업 요약
+    @classmethod
+    def summary(cls, request):
         try:
-            corp_name = json.loads(request.body)["corpName"]
+            corpName = json.loads(request.body)["corpName"]
             reportNum = json.loads(request.body)["reportNum"]
-            summary = Chatbot.getCorpSummary(corp_name, reportNum)
-            return JsonResponse({"message": f"Summary: {summary}"}, status=200)
-        except:
-            return HttpResponseBadRequest("Invalid Request")
+            dart = Dart()
+            dart_list = dart.getCorpList()
+            isDart = dart_check(corpName, dart_list)
+
+            chatbot = Chatbot()
+            response = chatbot.getCorpSummary(corpName, isDart, reportNum)
+
+            return JsonResponse({"summary": response}, status=200)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
     
-    def report(self, request):
-        # 특정 기업 보고서 원문 url 출력
+    # 기업 리스트
+    @classmethod
+    def corporations_list(cls, request):
         try:
-            isDart = True
-            corp_name = json.loads(request.body)["corpName"]
+            corpName = json.loads(request.body)["corpName"]
+            dart = Dart()
+            dart_list = dart.getCorpList()
+            isDart = dart_check(corpName, dart_list)
+            
+            chatbot = Chatbot()
+            list = chatbot.getCorpList(isDart)
+
+            return JsonResponse({"list": list}, status=200)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+    # 특정 기업 보고서 원문 url 출력
+    def report(cls, request):
+        try:
+            corpName = json.loads(request.body)["corpName"]
+            dart = Dart()
+            dart_list = dart.getCorpList()
+            isDart = dart_check(corpName, dart_list)
             fromDate = json.loads(request.body)["fromDate"]
             toDate = json.loads(request.body)["toDate"]
-            list = Chatbot.getCorpList(isDart, corp_name, date)
-            return JsonResponse({"message": f"List: {list}"}, status=200)
-        except:
-            return HttpResponseBadRequest("Invalid Request")
-        
-    def compare(self, request):
-        # 보고서끼리 비교
+
+            chatbot = Chatbot()
+            link = chatbot.getCorpReport(corpName, isDart, (fromDate, toDate))
+
+            return JsonResponse({"link": link}, status=200)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+    
+    # 보고서 비교
+    def compare(cls, request):
         try:
-            isDart = True
             corpName = json.loads(request.body)["corpName"]
             targetCorpName = json.loads(request.body)["targetCorpName"]
-            report = Chatbot.getCorpList(isDart, corpList)
-            return JsonResponse({"message": f"Report: {report}"}, status=200)
-        except:
-            return HttpResponseBadRequest("Invalid Request")
-    
-class DartAPI(View):
-    @classmethod
-    def chatbot_response(cls, request):
-        try:
-            question = json.loads(request.body)["question"]
-            return JsonResponse({"message": f"Question: {question}, Answer: Dart chatbot response"}, status=200)
-        except:
-            return HttpResponseBadRequest("Invalid Request")
-    
-    def corporations_list(self):
+            dart = Dart()
+            dart_list = dart.getCorpList()
+            corp_isDart = dart_check(corpName, dart_list)
+            target_isDart = dart_check(targetCorpName, dart_list)
 
-        return JsonResponse({"message": "Dart corporations list"}, status=200)
+            # 같은 종류의 공시 데이터만 비교 가능
+            if (corp_isDart and target_isDart) or (not corp_isDart and not target_isDart):
+                chatbot = Chatbot()
+                report = chatbot.Compare2Corps((corpName, targetCorpName), corp_isDart)
+            else:
+                return JsonResponse({"message": "서로 다른 종류의 공시 데이터는 비교가 불가합니다."})
 
-    def summary(self, corporation):
-
-        return JsonResponse({"message": f"Dart summary for corporation: {corporation}"}, status=200)
-
-class EdgarAPI(View):
-    @classmethod
-    def chatbot_response(cls, request):
-        try:
-            question = json.loads(request.body)["question"]
-            return JsonResponse({"message": f"Question: {question}, Answer: Edgar chatbot response"}, status=200)
-        except:
-            return HttpResponseBadRequest("Invalid Request")
-
-    def corporations_list(self):
-
-        return JsonResponse({"message": "Edgar corporations list"}, status=200)
-
-    def summary(self, corporation):
-
-        return JsonResponse({"message": f"Edgar summary for corporation: {corporation}"}, status=200)
+            return JsonResponse({"report": report}, status=200)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
