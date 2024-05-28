d4dba3ac0eb04ae71a722cd8c8455c2068146611
minguk-cucu <74689888+minguk-cucu@users.noreply.github.com>
2024-05-28 17:11:50 +0900

    Feat : sqlite에 is_dart field 추가

--- BE/App/App/models.py
+++ BE/App/App/models.py
@@ -4,3 +4,4 @@ class App(models.Model):
     corp_name = models.CharField(max_length=30)
     corp_num = models.CharField(max_length=30)
     corp_jamo = models.CharField(max_length=30)
+    is_dart = models.CharField(max_length=30)
